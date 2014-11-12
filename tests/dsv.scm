7d3a8a4bd954cd7e3b538c8b96ef7093ab462edd
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2014-11-12 04:14:53 +0300

    Add tests
    * tests/Makefile.am, tests/dsv.scm: Add to the repository.
    * Makefile.am (SUBDIRS): Add `tests'.
    * configure.ac (AM_INIT_AUTOMAKE): Enable `color-tests'.
      (AC_CONFIG_FILES): Add `tests/Makefile'.

---
+++ tests/dsv.scm
@@ -0,0 +1,32 @@
+;;; dsv.scm -- Tests for DSV parser.
+
+;; Copyright (C) 2014 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+(use-modules (srfi srfi-64)
+             (dsv))
+
+(test-begin "dsv")
+
+(test-assert "dsv-string->list"
+  (let ((test-string   "a:b:c")
+        (expected-list '("a" "b" "c")))
+    (equal? expected-list (dsv-string->list test-string))))
+
+(test-end "dsv")
+
+(exit (= (test-runner-fail-count (test-runner-current)) 0))
+
+;;; dsv.scm ends here.
