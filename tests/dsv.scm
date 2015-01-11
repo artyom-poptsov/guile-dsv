07b48bcf97c04d5df1f0b3b21ce56847600d8976
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-01-11 17:01:23 +0300

    tests/dsv.scm ("dsv-string->list, rfc4180"): Improve
    * tests/dsv.scm ("dsv-string->list, rfc4180"): Add a new TC, fix existing TCs.

--- tests/dsv.scm
+++ tests/dsv.scm
@@ -38,12 +38,14 @@
        (equal? '("" "b")
                (dsv-string->list "\"\",b" #\, #:format 'rfc4180))
        (equal? '(",a" "b")
-               (dsv-string->list "\",a\",b"))
+               (dsv-string->list "\",a\",b" #\, #:format 'rfc4180))
+       (equal? '("a," "b")
+               (dsv-string->list "\"a,\",b" #\, #:format 'rfc4180))
        (equal? '("a" "b")
                (dsv-string->list "\"a\",b" #\, #:format 'rfc4180))
        (equal? '("a,b" "c")
                (dsv-string->list "\"a,b\",c" #\, #:format 'rfc4180))
-       (equal? '("\"a\",b")
+       (equal? '("\"a\"" "b")
                (dsv-string->list "\"\"\"a\"\"\",b" #\, #:format 'rfc4180))))
 
 (test-assert "list->dsv-string"
