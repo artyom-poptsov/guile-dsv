27a7aa481010da9eb8a519eef259728b24f33113
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-08-14 17:09:35 +0300

    modules/dsv (dsv->scm, dsv-string->scm): Explicitly use "syslog"
    * modules/dsv.scm (dsv->scm, dsv-string->scm): Explicitly use "syslog" as the
      logging driver by default.

--- modules/dsv.scm
+++ modules/dsv.scm
@@ -85,7 +85,7 @@
                    (format         'unix)
                    (comment-prefix 'default)
                    (debug-mode?    #f)
-                   (log-driver     #f)
+                   (log-driver     "syslog")
                    (log-opt        '()))
   "Read DSV data from a PORT.  If the PORT is not set, read from the default
 input port.  If a DELIMITER is not set, use the default delimiter for a
@@ -114,7 +114,7 @@ values, or throw 'dsv-parser-error' on an error."
                           (format 'unix)
                           (comment-prefix 'default)
                           (debug-mode?    #f)
-                          (log-driver     #f)
+                          (log-driver     "syslog")
                           (log-opt        '()))
   "Convert a DSV string STR to a list of values using a DELIMITER.  If the
 DELIMITER is not set, use the default delimiter for a FORMAT.  Skip lines
