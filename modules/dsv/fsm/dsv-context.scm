d9c8f411b302ffecb8266e47866973c8767d3d72
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2026-01-05 00:29:10 +0300

    dsv/fsm/dsv-context: Bugfix
    * modules/dsv/fsm/dsv-context.scm: Bugfix: Add missing (scheme documentation)
    module.

--- modules/dsv/fsm/dsv-context.scm
+++ modules/dsv/fsm/dsv-context.scm
@@ -32,6 +32,7 @@
   #:use-module (ice-9 threads)
   #:use-module (dsv fsm context)
   #:use-module (dsv builder)
+  #:use-module (scheme documentation)
   #:autoload (fibers) (spawn-fiber)
   #:autoload (fibers channels) (make-channel get-message put-message)
   #:export (none
